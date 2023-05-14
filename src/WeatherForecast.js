391b358b40488f11556097dc76c313c43839277b
Yarden Segal <yarden.segal@gmail.com>
2023-05-14 21:46:15 +0200

    Added the live forecast data

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -9,21 +9,35 @@ export default function WeatherForecast(props) {
 
   function handleSubmit(response) {
     setForecastDay(response.data.daily);
-
     setLoaded(true);
   }
+
+  useEffect(
+    function () {
+      setLoaded(false);
+    },
+    [props.data.coordinates]
+  );
 
   if (loaded) {
     return (
       <div className="row">
-        <div className="col">
-          <WeatherForecastDay daily={forecastDay} />
-        </div>{" "}
+        {forecastDay.map(function (day, index) {
+          if (index < 5) {
+            return (
+              <div className="col" key={index}>
+                <WeatherForecastDay daily={day} />
+              </div>
+            );
+          } else {
+            return null;
+          }
+        })}
       </div>
     );
   } else {
     let apiKey = `50fa4024e3b1d5eac2f51ab18a47e997`;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.data.lat}&lon=${props.data.lon}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.data.coordinates.lat}&lon=${props.data.coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleSubmit);
     return "loading";
   }
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,17 +1,17 @@
 import Icon from "./Icon";
 export default function WeatherForecastDay(props) {
   function maxTemp() {
-    let maxTemp = Math.round(props.daily[0].temp.max);
+    let maxTemp = Math.round(props.daily.temp.max);
     return `${maxTemp}°`;
   }
 
   function minTemp() {
-    let minTemp = Math.round(props.daily[0].temp.min);
+    let minTemp = Math.round(props.daily.temp.min);
     return `${minTemp}°`;
   }
 
   function daily() {
-    let date = new Date(props.daily[0].dt * 1000);
+    let date = new Date(props.daily.dt * 1000);
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     let day = days[date.getDay()];
     return day;
@@ -21,7 +21,7 @@ export default function WeatherForecastDay(props) {
     <div className="forecast-day-wrapper">
       <div>{daily()}</div>
       <div className="icon-forecast">
-        <Icon icon={props.daily[0].weather[0].icon} size={30} />
+        <Icon icon={props.daily.weather[0].icon} size={30} />
       </div>
       <span className="degrees-min">{minTemp()} </span>
       <span className="degrees-max">{maxTemp()} </span>
